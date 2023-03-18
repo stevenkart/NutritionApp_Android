243c86bd352ba79fcd09edd6d36167bcea17bbc5
kart1396 <stevencjps4@gmail.com>
2023-03-17 21:12:30 -0600

    creacion del proyecto app

---
+++ NutritionApp_Android/NutritionApp_Android/GlobalObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NutritionApp_Android
+{
+    public class GlobalObjects
+    {
+        public static string MimeType = "application/json";
+        public static string ContentType = "Content-Type";
+
+
+        //agregar usuario global igual que en progra 5
+
+        //public static UserDTO LocalUser = new UserDTO();
+
+    }
+}
