6ae8c195723eb5f92bf1385a90853b68b62f1786
kart1396 <stevencjps4@gmail.com>
2023-04-22 21:51:09 -0600

    se actualiza branch 22/4/23

--- NutritionApp_Android/NutritionApp_Android/Views/ManageMyAccountDetails.xaml.cs
+++ NutritionApp_Android/NutritionApp_Android/Views/ManageMyAccountDetails.xaml.cs
@@ -42,14 +42,19 @@ namespace NutritionApp_Android.Views
 
         private async void BtnUpdate_Clicked(object sender, EventArgs e)
         {
+            string error = ValidateData();
+
             if (
-               TxtFullName.Text != null && !string.IsNullOrEmpty(TxtFullName.Text.Trim()) &&
-               TxtPhone.Text != null && !string.IsNullOrEmpty(TxtPhone.Text.Trim()) &&
-               TxtEmail.Text != null && !string.IsNullOrEmpty(TxtEmail.Text.Trim()) &&
-               TxtWeight.Text != null && !string.IsNullOrEmpty(TxtWeight.Text.Trim()) &&
-               TxtHeight.Text != null && !string.IsNullOrEmpty(TxtHeight.Text.Trim()) &&
-               TxtAge.Text != null && !string.IsNullOrEmpty(TxtAge.Text.Trim()) &&
-               TxtFat.Text != null && !string.IsNullOrEmpty(TxtFat.Text.Trim()) 
+                 /*
+                TxtFullName.Text != null && !string.IsNullOrEmpty(TxtFullName.Text.Trim()) &&
+                TxtPhone.Text != null && !string.IsNullOrEmpty(TxtPhone.Text.Trim()) &&
+                TxtEmail.Text != null && !string.IsNullOrEmpty(TxtEmail.Text.Trim()) &&
+                TxtWeight.Text != null && !string.IsNullOrEmpty(TxtWeight.Text.Trim()) &&
+                TxtHeight.Text != null && !string.IsNullOrEmpty(TxtHeight.Text.Trim()) &&
+                TxtAge.Text != null && !string.IsNullOrEmpty(TxtAge.Text.Trim()) &&
+                TxtFat.Text != null && !string.IsNullOrEmpty(TxtFat.Text.Trim())
+                 */
+                 string.IsNullOrEmpty(error)
                )
             {
 
@@ -162,6 +167,7 @@ namespace NutritionApp_Android.Views
                     }
                 }
             }
+            /*
             else
             {
                 //estas validaciones deben ser puntuales para informar al usuario que falla 
@@ -209,6 +215,55 @@ namespace NutritionApp_Android.Views
                     return;
                 }  
             }
+            */
+        }
+
+        // method -> data can't be empty
+        private string ValidateData()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (string.IsNullOrEmpty(TxtFullName.Text.Trim()))
+            {
+                sb.Append("Full Name field is empty \n");
+            }
+
+            if (string.IsNullOrEmpty(TxtPhone.Text.Trim()))
+            {
+                sb.Append("Phone field is empty \n");
+            }
+
+            if (string.IsNullOrEmpty(TxtEmail.Text.Trim()))
+            {
+                sb.Append("Email field is empty \n");
+            }
+
+            if (string.IsNullOrEmpty(TxtWeight.Text.Trim()))
+            {
+                sb.Append("Weight field is empty \n");
+            }
+
+            if (string.IsNullOrEmpty(TxtHeight.Text.Trim()))
+            {
+                sb.Append("Height field is empty \n");
+            }
+
+            if (string.IsNullOrEmpty(TxtAge.Text.Trim()))
+            {
+                sb.Append("Age field is empty \n");
+            }
+
+            if (string.IsNullOrEmpty(TxtFat.Text.Trim()))
+            {
+                sb.Append("Fat field is empty \n");
+            }
+
+            //if ( Validaciones.IsValidEmail( TxtEmail.Text.Trim() ) )
+            //{
+            //    sb.Append( "Email has not a well format \n" );
+            //}
+
+            return sb.ToString();
         }
 
         private async void BtnCancel_Clicked(object sender, EventArgs e)
